49e75e301f956d5336debc83eabb604955a1df23
bsabillon <bryan.sabillon@hotmail.com>
2020-06-08 18:02:20 -0600

    Trip form validations
    Added driver, vehicle and passengers select menu to tripForm

--- src/app/components/operations/trips/add/add.page.ts
+++ src/app/components/operations/trips/add/add.page.ts
@@ -3,28 +3,62 @@ import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 
-import {TripsService} from '../../../../services/trips.service';
+import { TripsService } from '../../../../services/trips.service';
+import { VehiclesService } from '../../../../services/vehicles.service';
+import { DriversService } from '../../../../services/drivers.service';
+import { PassengersService } from '../../../../services/passengers.service';
+import { Vehicle } from 'src/app/models/vehicle.class';
 
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.page.html',
   styleUrls: ['./add.page.scss'],
+  providers: [VehiclesService]
 })
 export class AddPage implements OnInit {
 
-  constructor(public tripsServices: TripsService) { }
+  constructor(public tripsServices: TripsService, public vehiclesServices: VehiclesService,
+    public driversServices: DriversService,
+    public passengersServices: PassengersService,
+  ) { }
+  public vehicles: any = [];
+  public drivers: any[];
+  public passengers: any[];
+  public trips: any = [];
 
   ngOnInit() {
+    this.getVehicules();
+    this.getDrivers();
+    this.getPassengers();
+
   }
 
-  addTrip(formTrip: NgForm):void{
-    if(formTrip.value.id == null){
+  addTrip(formTrip: NgForm): void {
+    if (formTrip.value.id == null) {
       this.tripsServices.addTrip(formTrip.value);
-    }else{
+    } else {
       this.tripsServices.updateTrip(formTrip.value);
     }
     formTrip.reset();
+  }
+
+  getVehicules() {
+    this.vehiclesServices.getVehicles().subscribe(vehicles => {
+      this.vehicles = vehicles;
+    });
+  };
+
+  getDrivers() {
+    this.driversServices.getDrivers().subscribe(drivers => {
+      this.drivers = drivers;
+    });
+  };
+
+  getPassengers() {
+    this.passengersServices.getPassengers().subscribe(passengers => {
+      this.passengers = passengers;
+    });
   }
 
 
